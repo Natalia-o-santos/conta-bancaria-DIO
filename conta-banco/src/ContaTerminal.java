74871065f8208424053b40bc2d56c335828be7a1
Natalia.o.santos <natalia.o.santos.00@gmail.com>
2024-05-29 22:43:00 -0300

    conta bancaria finalizada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        double saldo = 485.25;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da sua conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o numero da sua agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia +" conta " + numero + " e seu saldo " + saldo + " já está disponível para saque!");
+
     }
 }
